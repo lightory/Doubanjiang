3cb3c99de226437fa7a53aa75e5b78ae5ae43ad3
Light <light@huohua.in>
2013-09-28 13:45:07 +0800

    Init Project.

---
+++ src/net/lightory/doubanjiang/HomeActivity.java
@@ -0,0 +1,22 @@
+package net.lightory.doubanjiang;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+}
